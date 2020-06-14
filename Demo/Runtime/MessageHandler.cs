6b34ae5b5e602f029863965e013ebd8b0ec19906
lumpn <lumpn@users.noreply.github.com>
2020-06-14 10:32:40 +0000

    Added demo

---
+++ Demo/Runtime/MessageHandler.cs
@@ -0,0 +1,48 @@
+//----------------------------------------
+// MIT License
+// Copyright(c) 2020 Jonas Boetel
+//----------------------------------------
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Lumpn.Messaging.Demo
+{
+    public sealed class MessageHandler : MonoBehaviour, IMessageReceiver
+    {
+        [System.Serializable]
+        public struct Handler
+        {
+            public Message message;
+            public UnityEvent @event;
+        }
+
+        [SerializeField] private Handler[] handlers;
+
+        void OnEnable()
+        {
+            foreach (var handler in handlers)
+            {
+                handler.message.Register(this);
+            }
+        }
+
+        void OnDisable()
+        {
+            foreach (var handler in handlers)
+            {
+                handler.message.Deregister(this);
+            }
+        }
+
+        public void OnMessage(Message message)
+        {
+            foreach (var handler in handlers)
+            {
+                if (handler.message == message)
+                {
+                    handler.@event.Invoke();
+                }
+            }
+        }
+    }
+}
